b40015eba62ec8277eab187488d78c048add3077
Adenilson Veneranda da Silva <168023287+adenilsoon@users.noreply.github.com>
2025-02-02 21:25:45 -0300

    Criação de novos códigos de acompanhamento de aula. Uso de "while" e "MathRandom"

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,25 +1,5 @@
-// Atividade 01 de fixação da aula 01
-
-/*
-    alert("Boas vindas ao nosso site!");
-    let nome = "Lua";
-    let idade = 25;
-    let numeroDeVendas = 50;
-    let saldoDispoivel = 1000;
-    alert("Erro! Preencha todos os campos");
-    let mensagemDeErro = "Erro! Preencha todos os campos";
-    alert(mensagemDeErro);
-    nome = prompt("Informe o seu nome de usuário: ");
-    idade = prompt("Informe sua idade: ");
-
-    if(idade >= 18) {
-        alert("Pode tirar a habilitação!");
-    }
-*/
-
 // Acompanhamento aulas abaixo
 
-/*
 alert("Boas vindas ao jogo do número secreto!");
 let numeroSecreto = 8;
 console.log(numeroSecreto);
