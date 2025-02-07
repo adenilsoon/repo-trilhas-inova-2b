3aeb2551a1e02dcc0d33211fc90ee97348eaca6c
Adenilson Veneranda da Silva <168023287+adenilsoon@users.noreply.github.com>
2025-02-07 11:29:41 -0300

    Finalizando Jogo do Número Secreto

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -4,6 +4,8 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um numero entre 1 e 100';
 
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,6 +19,7 @@ exibirMensagemInicial();
 function exibirTextoNaTela(tag, texto){
      let campo = document.querySelector(tag);
      campo.innerHTML = texto;
+     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function verificarChute() {
@@ -45,7 +48,19 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio () {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    
+    if(quantidadeDeElementosNaLista == numeroEscolhido){
+        listaDeNumerosSorteados = [];
+    }
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo(){
